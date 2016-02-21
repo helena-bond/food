9607c95d6fdaf92b2a77970da622b5d66e88ed56
Helena Bond <crazypunkbaby@gmail.com>
2016-02-21 19:27:07 +0200

    Current data for one box

--- routes/index.js
+++ routes/index.js
@@ -14,23 +14,33 @@ router.get('/', suspend.promise(function *(req, res, next) {
     // var per_page = parseInt(query.per_page) || 30;
     // var page = parseInt(query.page) || 1;
 
-    var boxes = yield boxModel.find({});
-
+    var boxes = yield boxModel.find({}).sort({ date: -1 }).limit(50);
+    var fields = [];
     for (var i in boxes) {
-      var info = {};
       var box = boxes[i]._doc;
       for (var j in box) {
-        if (['id', '_id', 'external_id', 'type', 'date', '__v'].indexOf(j) < 0) {
-          info[j] = box[j];
+        if ((['id', '_id', 'external_id', 'type', 'date', '__v'].indexOf(j)) < 0 && (fields.indexOf(j) < 0)) {
+          fields.push(j);
         }
       }
-      boxes[i]._info = JSON.stringify(info);
+    }
+
+    var current_data = '';
+    if (boxes && boxes[0] && boxes[0]._doc) {
+      for (var i in fields) {
+        if (boxes[0]._doc[fields[i]]) {
+          if (current_data.length > 0) current_data += ', ';
+          current_data += fields[i] + ': ' + boxes[0]._doc[fields[i]];
+        }
+      }
     }
 
     res.render('index', {
-      title: 'Boxes info',
+      title: 'Box info',
       // page: page,
       // per_page: per_page,
+      fields: fields,
+      current_data: current_data,
       boxes: boxes
     });
 
